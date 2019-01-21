dfffafc5d12e3850ad3bda07d0ebc4ba9f6b17f1
[choisojung] <[sojc31@gmail.com]>
2019-01-21 09:29:40 -0800

    updated php

--- formhandler.php
+++ formhandler.php
@@ -1,17 +1,19 @@
 <?php
 	
 
-if ($_GET && isset($_GET['submit'], $_GET['name'], $_GET['email'],$_GET['tel'],$_GET['state'])){
     $name = $_GET['name'];
     $email = $_GET['email'];
     $phone = $_GET['tel'];
     $state = $_GET['state'];
     
-    echo $name;
-    echo $email;
-    echo $phone;
-    echo $state;
-}
-    
+    echo "Name: $name<br>";
+    echo "Email: $email<br>";
+    if ($phone != "") {
+        echo "Phone: $phone<br>";
+    }
+    if ($state !="") {
+        echo "State: $state<br>";
+    }
+
 
 ?>
